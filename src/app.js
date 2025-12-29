c18a0f60fe93ed7fcb0ee8eb2edf5d3c6d390c7a
Arthur Manenti <arthur.manenti.videira@gmail.com>
2025-12-29 15:02:40 -0300

    Arquivo de rotas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,10 @@
 const express = require("express");
-
 const app = express();
 const PORT = 8000;
 
-app.get("/", (req, res) => {
-  res.json({ oi: "oi" });
-});
+const rotasPrincipais = require("./routes/routes.js");
+
+app.use("/", rotasPrincipais);
 
 app.listen(PORT, () => {
   console.log(`Ouvindo porta: ${PORT}`);

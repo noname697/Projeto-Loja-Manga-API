cfbfedfa606e981c67d075cc70bfce2ec4632412
Arthur Manenti <arthur.manenti.videira@gmail.com>
2025-12-30 14:45:53 -0300

    CORS

--- src/app.js
+++ src/app.js
@@ -1,6 +1,14 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const PORT = 8000;
+
+const corsOptions = {
+  origin: "http://localhost:3000",
+  methods: "GET,POST,PUT,DELETE",
+};
+
+app.use(cors(corsOptions));
 
 const rotasCompras = require("./routes/comprasRotas.js");
 const rotasUsuarios = require("./routes/usuariosRotas.js");
